7ba87539aa32b91947fa9924447701477baafab3
Jonas Wielicki <j.wielicki@sotecware.net>
2015-04-08 13:24:42 +0200

    Move to yet another terrain rendering algorithm

---
+++ engine/engine/render/fancyterrain.hpp
@@ -0,0 +1,140 @@
+#ifndef SCC_ENGINE_RENDER_FANCYTERRAIN_H
+#define SCC_ENGINE_RENDER_FANCYTERRAIN_H
+
+#include <unordered_map>
+
+#include "engine/render/scenegraph.hpp"
+
+#include "engine/sim/terrain.hpp"
+
+namespace engine {
+
+struct HeightmapSliceMeta
+{
+    unsigned int basex;
+    unsigned int basey;
+    unsigned int lod;
+
+    bool operator<(const HeightmapSliceMeta &other) const;
+
+};
+
+}
+
+namespace std {
+
+template<>
+struct hash<engine::HeightmapSliceMeta>
+{
+    typedef engine::HeightmapSliceMeta argument_type;
+    typedef typename std::hash<unsigned int>::result_type result_type;
+
+    hash():
+        m_uint_hash()
+    {
+
+    }
+
+private:
+    hash<unsigned int> m_uint_hash;
+
+public:
+    result_type operator()(const argument_type &value) const
+    {
+        return m_uint_hash(value.basex)
+                ^ m_uint_hash(value.basey)
+                ^ m_uint_hash(value.lod);
+    }
+
+};
+
+}
+
+namespace engine {
+
+/**
+ * Scenegraph node which renders a terrain using the CDLOD algorithm by
+ * Strugar.
+ *
+ * It is mainly controlled by the ``grid_size`` and ``texture_cache_size``
+ * parameters, as passed to the constructor.
+ *
+ * The *grid_size* controlls the number of vertices in a single grid tile used
+ * for terrain rendering. For the smallest tile, this is equivalent to the
+ * number of heightmap points covered. This is thus the world size of the
+ * smallest (i.e. most precise, like with mipmaps) level-of-detail.
+ *
+ * For rendering, parts of the heightmap (and belonging normal maps etc.) are
+ * cached on the GPU, depending on which are needed. The size of the cache is
+ * controlled by *texture_cache_size*, which is the number of **tiles** along
+ * one axis of the cache texture. For a grid size of 64 and a texture cache
+ * size of 32, a square texture of size 2048 (along each edge) will be
+ * created.
+ */
+class FancyTerrainNode: public scenegraph::Node
+{
+public:
+    typedef unsigned int SlotIndex;
+
+public:
+    /**
+     * Construct a fancy terrain node.
+     *
+     * @param terrain The terrain to render.
+     * @param grid_size The length of an edge of the smallest square tile
+     * to render. Must divide the size of the terrain with a power of two.
+     * @param texture_cache_size The square root of the number of tiles which
+     * will be cached on the GPU. This will create a square texture with
+     * grid_size*texture_cache_size texels on each edge.
+     */
+    FancyTerrainNode(sim::Terrain &terrain,
+                     const unsigned int grid_size,
+                     const unsigned int texture_cache_size);
+
+private:
+    const unsigned int m_grid_size;
+    const unsigned int m_texture_cache_size;
+    const unsigned int m_max_lod;
+    const unsigned int m_min_lod;
+
+    sim::Terrain &m_terrain;
+    Texture2D m_heightmap;
+
+    VBO m_vbo;
+    IBO m_ibo;
+
+    Material m_material;
+
+    VBOAllocation m_vbo_allocation;
+    IBOAllocation m_ibo_allocation;
+
+    std::unique_ptr<VAO> m_vao;
+
+    std::unordered_map<HeightmapSliceMeta, SlotIndex> m_allocated_slices;
+    std::vector<SlotIndex> m_unused_slots;
+    std::vector<HeightmapSliceMeta> m_heightmap_slots;
+
+    std::vector<HeightmapSliceMeta> m_tmp_slices;
+
+protected:
+    void collect_slices_recurse(
+            std::vector<HeightmapSliceMeta> &requested_slices,
+            const Vector<unsigned int, 2> &center,
+            unsigned int lod,
+            const Vector3f &viewpoint);
+    void collect_slices(
+            std::vector<HeightmapSliceMeta> &requested_slices,
+            const Vector3f &viewpoint);
+    void compute_heightmap_lod(unsigned int basex,
+                               unsigned int basey,
+                               unsigned int lod);
+
+public:
+    void render(RenderContext &context) override;
+    void sync() override;
+
+};
+
+}
+
+#endif
---
+++ engine/src/render/fancyterrain.cpp
@@ -0,0 +1,163 @@
+#include "engine/render/fancyterrain.hpp"
+
+namespace engine {
+
+bool HeightmapSliceMeta::operator<(const HeightmapSliceMeta &other) const
+{
+    return lod < other.lod;
+}
+
+
+FancyTerrainNode::FancyTerrainNode(sim::Terrain &terrain,
+                                   const unsigned int grid_size,
+                                   const unsigned int texture_cache_size):
+    m_grid_size(grid_size),
+    m_texture_cache_size(texture_cache_size),
+    m_min_lod(terrain.m_width / grid_size),
+    m_max_lod(terrain.m_width),
+    m_terrain(terrain),
+    m_heightmap(GL_R32F,
+                texture_cache_size*grid_size,
+                texture_cache_size*grid_size,
+                GL_RED,
+                GL_FLOAT),
+    m_vbo(VBOFormat({
+                        VBOAttribute(2)
+                    })),
+    m_ibo(),
+    m_vbo_allocation(m_vbo.allocate(grid_size*grid_size)),
+    m_ibo_allocation(m_ibo.allocate(grid_size*grid_size*6))
+{
+    uint16_t *dest = m_ibo_allocation.get();
+    for (unsigned int y = 0; y < grid_size-1; y++) {
+        for (unsigned int x = 0; x < grid_size-1; x++) {
+            const unsigned int curr_base = y*grid_size + x;
+            *dest++ = curr_base + grid_size;
+            *dest++ = curr_base + grid_size + 1;
+            *dest++ = curr_base;
+
+            *dest++ = curr_base;
+            *dest++ = curr_base + grid_size + 1;
+            *dest++ = curr_base + 1;
+        }
+    }
+
+    m_heightmap.bind();
+    std::basic_string<float> buf(64*64, 20.);
+    raise_last_gl_error();
+    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_NEAREST);
+    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_NEAREST);
+    glTexSubImage2D(GL_TEXTURE_2D, 0, 0, 0, 64, 64, GL_RED, GL_FLOAT, buf.data());
+    raise_last_gl_error();
+
+    m_ibo_allocation.mark_dirty();
+
+    bool success = true;
+
+    success = success && m_material.shader().attach(
+                GL_VERTEX_SHADER,
+                "#version 330\n"
+                "layout(std140) uniform MatrixBlock {"
+                "   layout(row_major) mat4 proj;"
+                "   layout(row_major) mat4 view;"
+                "   layout(row_major) mat4 model;"
+                "   layout(row_major) mat3 normal;"
+                "} mats;"
+                "uniform float chunk_size;"
+                "uniform vec2 chunk_translation;"
+                "uniform vec2 heightmap_base;"
+                "uniform sampler2D heightmap;"
+                "in vec2 position;"
+                "out vec2 tc0;"
+                "void main() {"
+                "   tc0 = position.xy / 5.0;"
+                "   float height = textureLod(heightmap, heightmap_base + position.xy, 0).r;"
+                "   gl_Position = mats.proj * mats.view * mats.model * vec4("
+                "       position * chunk_size + chunk_translation, height, 1.f);"
+                "}");
+    success = success && m_material.shader().attach(
+                GL_FRAGMENT_SHADER,
+                "#version 330\n"
+                "out vec4 color;"
+                "in vec2 tc0;"
+                "uniform sampler2D heightmap;"
+                "void main() {"
+                "   color = vec4(0.5, 0.5, 0.5, 1.0);"
+                "}");
+    success = success && m_material.shader().link();
+
+    if (!success) {
+        throw std::runtime_error("failed to compile or link shader");
+    }
+
+    m_material.shader().bind();
+    glUniform1f(m_material.shader().uniform_location("chunk_size"),
+                64.0);
+    glUniform2f(m_material.shader().uniform_location("chunk_translation"),
+                0.0, 0.0);
+    glUniform2f(m_material.shader().uniform_location("heightmap_base"),
+                0.0, 0.0);
+    m_material.attach_texture("heightmap", &m_heightmap);
+
+    ArrayDeclaration decl;
+    decl.declare_attribute("position", m_vbo, 0);
+    decl.set_ibo(&m_ibo);
+
+    m_vao = decl.make_vao(m_material.shader(), true);
+
+    {
+        auto slice = VBOSlice<Vector2f>(m_vbo_allocation, 0);
+        unsigned int index = 0;
+        for (unsigned int y = 0; y < grid_size; y++) {
+            for (unsigned int x = 0; x < grid_size; x++) {
+                slice[index++] = Vector2f(x, y) / grid_size;
+            }
+        }
+    }
+    m_vbo_allocation.mark_dirty();
+
+    m_unused_slots.reserve(m_texture_cache_size*m_texture_cache_size);
+    m_heightmap_slots.resize(m_texture_cache_size*m_texture_cache_size);
+    for (unsigned int i = 0;
+         i < m_texture_cache_size*m_texture_cache_size;
+         i++)
+    {
+        m_unused_slots.emplace_back(i);
+    }
+}
+
+void FancyTerrainNode::collect_slices(
+        std::vector<HeightmapSliceMeta> &requested_slices,
+        const Vector3f &viewpoint)
+{
+    collect_slices_recurse(requested_slices,
+                           Vector<unsigned int, 2>(m_max_lod / 2, m_max_lod / 2),
+                           m_max_lod,
+                           viewpoint);
+}
+
+void FancyTerrainNode::collect_slices_recurse(
+        std::vector<HeightmapSliceMeta> &requested_slices,
+        const Vector<unsigned int, 2> &center,
+        unsigned int lod,
+        const Vector3f &viewpoint)
+{
+
+}
+
+void FancyTerrainNode::render(RenderContext &context)
+{
+    glPolygonMode(GL_FRONT_AND_BACK, GL_LINE);
+    context.draw_elements(GL_TRIANGLES, *m_vao, m_material, m_ibo_allocation);
+    glPolygonMode(GL_FRONT_AND_BACK, GL_FILL);
+}
+
+void FancyTerrainNode::sync()
+{
+    m_vao->sync();
+    m_heightmap.bind();
+    glTexSubImage2D(GL_TEXTURE_2D, 0, 0, 0, 64, 64, GL_RED, GL_FLOAT,
+                    m_terrain.m_heightmap.data());
+}
+
+}
