40f7f1c8d368fcb797f918e7543a3b78cf1e61e3
Jonas Wielicki <j.wielicki@sotecware.net>
2015-04-15 18:25:14 +0200

    Confine updates of FieldLODifier instances

--- engine/engine/sim/terrain.hpp
+++ engine/engine/sim/terrain.hpp
@@ -250,42 +250,64 @@ protected:
         auto source_lock = m_source.readonly_field(source_field);
 
         const Field *prev_heightfield = source_field;
-        Field next_heightfield;
         unsigned int prev_size = m_size;
+
+        // avoid re-allocation which breaks everything!
+        m_lods.reserve(m_lod_count-1);
+
+        TerrainRect to_update = updated;
 
         for (unsigned int i = 1; i < m_lod_count; i++)
         {
             const unsigned int this_size = (m_size >> i)+1;
-            next_heightfield.resize(this_size*this_size);
             lod_logger.logf(io::LOG_DEBUG, "generating LOD %u (size=%u)", i,
                             this_size);
-            element_t *dest_ptr = &next_heightfield.front();
 
-            for (unsigned int y = 0, src_y = 0; y < this_size; y++, src_y += 2)
+            Field *next_heightfield;
+            if ((m_lods.size()+1) <= i) {
+                // if this level has not been computed yet, force the rect to
+                // full size!
+                to_update = TerrainRect(
+                            0, 0,
+                            this_size, this_size);
+                read_lock.unlock();
+                write_lock.lock();
+                m_lods.emplace_back(Field(this_size*this_size));
+                next_heightfield = &m_lods[i-1];
+            } else {
+                // reduce rect size
+                to_update.set_x0(to_update.x0() / 2);
+                to_update.set_y0(to_update.y0() / 2);
+
+                // +1 for odd field size
+                to_update.set_x1(to_update.x1() / 2 + 1);
+                to_update.set_y1(to_update.y1() / 2 + 1);
+
+                next_heightfield = &m_lods[i-1];
+                read_lock.unlock();
+                write_lock.lock();
+            }
+
+            const unsigned int this_width = (to_update.x1() - to_update.x0());
+            const unsigned int this_height = (to_update.y1() - to_update.y0());
+
+            for (unsigned int y = 0, src_y = to_update.y0()*2;
+                 y < this_height;
+                 y++, src_y += 2)
             {
-                for (unsigned int x = 0, src_x = 0; x < this_size; x++, src_x += 2)
+                element_t *dest_ptr = &(*next_heightfield)[(to_update.y0()+y)*this_size+to_update.x0()];
+                for (unsigned int x = 0, src_x = to_update.x0()*2;
+                     x < this_width;
+                     x++, src_x += 2)
                 {
                     *dest_ptr++ = (*prev_heightfield)[src_y*prev_size+src_x];
                 }
             }
-            lod_logger.logf(io::LOG_DEBUG, "generated LOD %d, saving", i);
 
             if (source_field) {
                 source_field = nullptr;
                 source_lock.unlock();
                 prev_heightfield = nullptr;
-            }
-
-            read_lock.unlock();
-            // give other threads the chance to acquire the read lock right now
-            std::this_thread::yield();
-            write_lock.lock();
-
-            if ((m_lods.size()+1) <= i) {
-                m_lods.emplace_back(next_heightfield);
-            } else {
-                m_lods[i-1].swap(next_heightfield);
-                m_lods[i-1].shrink_to_fit();
             }
 
             write_lock.unlock();
