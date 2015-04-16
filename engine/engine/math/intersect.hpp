89c7ba670e51cb6797c5cffb746de3b434581d96
Jonas Wielicki <j.wielicki@sotecware.net>
2015-04-16 20:16:47 +0200

    Add comment on isect_ray_triangle for attribution

--- engine/engine/math/intersect.hpp
+++ engine/engine/math/intersect.hpp
@@ -4,7 +4,21 @@
 #include "engine/math/vector.hpp"
 #include "engine/math/shapes.hpp"
 
-
+/**
+ * Calculate the intersection point between a Ray and a triangle, the latter
+ * given by three Vector3f.
+ *
+ * The implementation is a verbatim port of the implementation from
+ * ["Fast, Minimum Storage Ray/Triangle Intersection" by Möller and Trumbore](http://www.cs.virginia.edu/~gfx/Courses/2003/ImageSynthesis/papers/Acceleration/Fast%20MinimumStorage%20RayTriangle%20Intersection.pdf),
+ * using the two-sided triangle version.
+ *
+ * @param ray Ray to intersect the triangle with
+ * @param p0 First point of the triangle
+ * @param p1 Second point of the triangle
+ * @param p2 Third point of the triangle
+ * @return Tuple consisting of the "time" \a t at which the ray hits the
+ * triangle and a boolean whether the ray hit the triangle.
+ */
 std::tuple<float, bool> isect_ray_triangle(
         const Ray &ray,
         const Vector3f &p0,
