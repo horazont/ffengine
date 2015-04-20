b2b9ff11e001a6bdcd0a283109eecc8012419474
Jonas Wielicki <j.wielicki@sotecware.net>
2015-04-20 19:06:02 +0200

    GPL license headers

--- engine/engine/io/mount.hpp
+++ engine/engine/io/mount.hpp
@@ -1,27 +1,25 @@
 /**********************************************************************
-File name: Mount.hpp
-This file is part of: Pythonic Engine
+File name: mount.hpp
+This file is part of: SCC (working title)
 
 LICENSE
 
-The contents of this file are subject to the Mozilla Public License
-Version 1.1 (the "License"); you may not use this file except in
-compliance with the License. You may obtain a copy of the License at
-http://www.mozilla.org/MPL/
+This program is free software: you can redistribute it and/or modify it under
+the terms of the GNU General Public License as published by the Free Software
+Foundation, either version 3 of the License, or (at your option) any later
+version.
 
-Software distributed under the License is distributed on an "AS IS"
-basis, WITHOUT WARRANTY OF ANY KIND, either express or implied. See the
-License for the specific language governing rights and limitations under
-the License.
+This program is distributed in the hope that it will be useful, but WITHOUT ANY
+WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR A
+PARTICULAR PURPOSE.  See the GNU General Public License for more details.
 
-Alternatively, the contents of this file may be used under the terms of
-the GNU General Public license (the  "GPL License"), in which case  the
-provisions of GPL License are applicable instead of those above.
+You should have received a copy of the GNU General Public License along with
+this program.  If not, see <http://www.gnu.org/licenses/>.
 
 FEEDBACK & QUESTIONS
 
-For feedback and questions about pyengine please e-mail one of the
-authors named in the AUTHORS file.
+For feedback and questions about SCC please e-mail one of the authors named in
+the AUTHORS file.
 **********************************************************************/
 #ifndef SCC_ENGINE_IO_MOUNT_H
 #define SCC_ENGINE_IO_MOUNT_H
